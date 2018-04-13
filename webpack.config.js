a76610258319908da59caf0c7002c20baec671c5
Yujie Zhu <yujiezhu718@gmail.com>
2018-04-13 14:42:52 -0700

    added plugins:plugins to config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,22 +27,23 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/venueShine.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['env', 'react']
+          presets: ["env", "react"]
         }
       }
     ]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", "*"]
   }
